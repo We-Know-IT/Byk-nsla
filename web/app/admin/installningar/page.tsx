5fac51ffa0542f4f457b3c743b4b441b80198db5
Oskar André <93657491+oskarandre@users.noreply.github.com>
2026-05-25 15:51:15 +0200

    fix: functions load bug

--- web/app/admin/installningar/page.tsx
+++ web/app/admin/installningar/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect } from "react";
 import { AdminSectionPage, adminNavItems } from "../../modules/admin";
 import ThemeSelector from "../../modules/admin/components/theme-selector";
 import StartPageSectionsSelector from "../../modules/admin/components/start-page-sections-selector";
@@ -6,6 +9,17 @@ import SectionHeader from "../../shared/ui/section-header";
 import { SettingsProvider } from "../../modules/admin/components/settings-context";
 
 export default function Page() {
+    useEffect(() => {
+        const handlePageShow = (event: PageTransitionEvent) => {
+            if (event.persisted) {
+                window.location.reload();
+            }
+        };
+
+        window.addEventListener("pageshow", handlePageShow);
+        return () => window.removeEventListener("pageshow", handlePageShow);
+    }, []);
+
     return (
         <AdminSectionPage activeKey="installningar" navItems={adminNavItems}>
             <div className="relative flex flex-col pt-1">
