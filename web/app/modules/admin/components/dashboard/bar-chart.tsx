90de5b0c8b784ad4551b8cb2596cfb77354c3ac1
Oskar André <93657491+oskarandre@users.noreply.github.com>
2026-06-11 18:43:20 +0200

    feat: bar chart for admin dashboard

---
+++ web/app/modules/admin/components/dashboard/bar-chart.tsx
@@ -0,0 +1,58 @@
+
+
+export default function BarChart({
+    data,
+    title,
+    text,
+    dataTitle,
+}: {
+    data?: { label: string; value: number }[];
+    title?: string;
+    text?: string;
+    dataTitle?: string;
+}
+
+) {
+
+    return (
+        <div className="flex flex-col h-64 w-full rounded-xl bg-surface p-4 border border-border shadow-md">
+            <div className="flex flex-row items-center justify-between mb-4">
+                <div>
+                    <h2 className="text-lg font-bold text-foreground">{title || "Data"}</h2>
+                    {text && <p className="text-sm text-foreground-muted">{text}</p>}
+                </div>
+
+                <div className="flex items-center gap-2">
+                    <div className="size-3 bg-brand-secondary rounded-sm"/>
+                    {dataTitle && <p className="text-sm text-foreground-muted">{dataTitle}</p>}
+                </div>
+            </div>
+
+
+            {data && data.length > 0 ? (
+                <div className="flex flex-row items-end justify-around w-full h-full gap-2">
+                    {data.map((item, index) => {
+                        const maxVal = Math.max(...data.map(d => d.value)) || 1;
+                        const barHeight = (item.value / maxVal) * 80;
+                        return (
+                            <div className="flex flex-1 flex-col justify-end h-full max-w-3xs gap-1" key={index} >
+                                <div
+                                    style={{ height: `${barHeight}%` }}
+                                    className="bg-brand-secondary rounded-t-xl min-h-1"
+                                />
+                                <p className="text-xs text-foreground-muted text-center">{item.label}</p>
+                            </div>
+                        );
+                    })}
+                </div>
+
+
+            ) : (
+                <div className="flex items-center justify-center w-full h-full rounded-xl bg-gray-200 p-4">
+                    <h2 className="text-lg font-medium text-foreground-muted mb-4">Ingen data tillgänglig</h2>
+                </div>
+            )}
+        </div>
+
+    );
+}
