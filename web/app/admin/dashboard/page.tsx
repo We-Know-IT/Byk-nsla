4fc4eef9d52d06ab3c9218c7389cc125e2380b7a
Oskar André <93657491+oskarandre@users.noreply.github.com>
2026-06-12 11:08:03 +0200

    feat: bar chart is hoverable, displays data

--- web/app/admin/dashboard/page.tsx
+++ web/app/admin/dashboard/page.tsx
@@ -9,9 +9,9 @@ const mockData = [
     { label: 'Januari', value: 30 },
     { label: 'Februari', value: 20 },
     { label: 'Mars', value: 50 },
-    { label: 'April', value: 40 },
+    { label: 'April', value: 60 },
     { label: 'Maj', value: 60 },
-    { label: 'Juni', value: 35 },
+    { label: 'Juni', value: 20 },
     { label: 'Juli', value: 32 },
     { label: 'Augusti', value: 25 },
 ];
@@ -31,13 +31,13 @@ export default function Page() {
       <div className="relative flex flex-col md:flex-row md:gap-4">
         <StatsBox title="Totala användare" value={1424} goal={2000} previousValue={1300} iconSrc="/icons/Frame.svg" />
         <StatsBox title="Aktiva byar & städer" value={18} iconSrc="/icons/House.svg" />
-        <StatsBox title="Inlägg denna månad" value={836} previousValue={950} iconSrc="/icons/Message.svg" />
+        <StatsBox title="Inlägg denna månad" value={836} goal={950} goalText="Jämfört med förra månaden" previousValue={950} iconSrc="/icons/Message.svg" />
         <StatsBox title="Kommande event" value={27} iconSrc="/icons/Calendar_detailed.svg" />
 
       </div>
 
 
-      <div className="grid md:grid-cols-10 md:gap-4">
+      <div className="flex flex-col md:grid md:grid-cols-10 gap-4">
         <div className="md:col-span-7">
         <BarChart data={mockData} title="Nya användare per månad" text="Registreringar under 2026" dataTitle="Användare" />
         </div>
--- web/app/modules/admin/components/dashboard/bar-chart.tsx
+++ web/app/modules/admin/components/dashboard/bar-chart.tsx
@@ -1,4 +1,6 @@
 
+import React from "react";
+import { cn } from "../../../../shared/utils/cn";
 
 export default function BarChart({
     data,
@@ -13,6 +15,7 @@ export default function BarChart({
 }
 
 ) {
+    const [hoveredBar, setHoveredBar] = React.useState<number | null>(null);
 
     return (
         <div className="flex flex-col h-64 w-full rounded-xl bg-surface p-4 border border-border shadow-md">
@@ -20,30 +23,57 @@ export default function BarChart({
                 <div>
                     <h2 className="text-md font-medium text-foreground">{title || "Data"}</h2>
                     {text && <p className="text-sm text-foreground-muted">{text}</p>}
+
+                    {hoveredBar !== null && data && (
+                        <div className="bg-surface border border-border rounded-md p-1 text-xs text-foreground shadow-sm absolute mt-1">
+                            <p>{data[hoveredBar].label}: {data[hoveredBar].value} {dataTitle}</p>
+                        </div>
+                    )}
+
                 </div>
 
                 <div className="flex items-center gap-2">
-                    <div className="size-3 bg-brand-secondary rounded-sm"/>
+                    <div className="size-3 bg-brand-secondary rounded-sm" />
                     {dataTitle && <p className="text-sm text-foreground-muted">{dataTitle}</p>}
                 </div>
             </div>
 
 
             {data && data.length > 0 ? (
-                <div className="flex flex-row items-end justify-around w-full h-full gap-2">
+                <div className="flex flex-row items-end justify-around w-full h-full gap-2 overflow-x-scroll">
                     {data.map((item, index) => {
                         const maxVal = Math.max(...data.map(d => d.value)) || 1;
                         const barHeight = (item.value / maxVal) * 80;
                         return (
-                            <div className="flex flex-1 flex-col justify-end h-full max-w-3xs gap-1" key={index} >
+
+
+                            <div className="flex flex-1 flex-col justify-end h-full min-w-15 max-w- gap-1" key={index} >
                                 <div
                                     style={{ height: `${barHeight}%` }}
-                                    className="bg-brand-secondary rounded-t-xl min-h-1"
+                                    className={cn("bg-brand-secondary rounded-t-xl min-h-1",
+                                        hoveredBar === index && "scale-105 border-3 border-brand-third transition-transform duration-200"
+                                    )}
+                                    onMouseEnter={(e) => {
+                                        setHoveredBar(index);
+                                    }}
+                                    onMouseLeave={() => {
+                                        setHoveredBar(null);
+                                    }}
+                                    onClick={() => { setHoveredBar(index); }}
+
                                 />
                                 <p className="text-xs text-foreground-muted text-center">{item.label}</p>
+
                             </div>
+
+
+
+
+
                         );
                     })}
+
+
                 </div>
 
 
